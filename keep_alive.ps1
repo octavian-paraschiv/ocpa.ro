67960382fa8ba0acf51efefd03590af6429948dd
octavianparaschiv <octavian.paraschiv@cegeka.com>
2022-10-31 17:08:07 +0200

    a

--- keep_alive.ps1
+++
@@ -1,5 +0,0 @@
-Invoke-WebRequest -URI https://ocpa.ro/api/meteo/data?region=Europe&subregion=Serbia&city=Belgrade&skip=0&take=0
-
-Invoke-WebRequest -URI https://ocpa.ro/api/meteo/data?region=Romania&subregion=Constanta&city=Constanta&skip=0&take=0
-
-Invoke-WebRequest -URI https://ocpa.ro/api/meteo/data?region=Romania&subregion=Timis&city=Timisoara&skip=0&take=0
