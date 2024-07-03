fc2498994690fc59bb11f6768ccc1ae22251ba74
Octavian Paraschiv <octavian.paraschiv@gmail.com>
2024-07-03 09:28:58 +0300

    no message

--- api/Controllers/ContentController.cs
+++ api/Controllers/ContentController.cs
@@ -8,6 +8,7 @@ using ocpa.ro.api.Helpers.Generic;
 using ocpa.ro.api.Models.Content;
 using ocpa.ro.api.Policies;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ocpa.ro.api.Controllers
@@ -27,6 +28,17 @@ namespace ocpa.ro.api.Controllers
         {
             _contentHelper = contentHelper ?? throw new ArgumentNullException(nameof(contentHelper));
             _multipartHelper = multipartHelper ?? throw new ArgumentNullException(nameof(multipartHelper));
+        }
+
+        [HttpGet("paths")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult GetContentRootPath()
+        {
+            return Ok(new Dictionary<string, string>
+            {
+                { "_hostingEnvironment.ContentRootPath", _hostingEnvironment.ContentRootPath },
+                { "_hostingEnvironment.ContentPath()", _hostingEnvironment.ContentPath() }
+            });
         }
 
         [HttpGet("{*contentPath}")]
