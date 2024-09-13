95637eaa172f0e66d010740015224d3b3fd5fdc4
Octavian Paraschiv <octavian.paraschiv@gmail.com>
2024-09-13 10:56:46 +0300

    Add ability to provide backend version via Utility API

---
+++ api/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ocpa.ro.api;
+
+public static class Constants
+{
+    public const string AppName = "ocpa.ro website";
+    public const string CompanyName = "OPMedia Research";
+    public const string CopyrightNotice = "Copyright © " + CompanyName;
+    public const string Version = "1.0.0.0";
+}
---
+++ api/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Constants = ocpa.ro.api.Constants;
+
+
+[assembly: AssemblyTitle(Constants.AppName)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany(Constants.CompanyName)]
+[assembly: AssemblyProduct(Constants.AppName)]
+[assembly: AssemblyCopyright(Constants.CopyrightNotice)]
+[assembly: AssemblyVersion(Constants.Version)]
+[assembly: AssemblyFileVersion(Constants.Version)]
