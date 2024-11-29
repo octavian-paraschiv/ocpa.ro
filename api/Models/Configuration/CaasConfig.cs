d141389340c4e009378df403a36b510ed29eb2b1
Octavian Paraschiv <octavian.paraschiv@gmail.com>
2024-11-29 12:15:29 +0200

    Cat As A Service reworked x4

--- api/Models/Configuration/CaasConfig.cs
+++ api/Models/Configuration/CaasConfig.cs
@@ -3,7 +3,7 @@
     public class CaasConfig
     {
         public static readonly string SectionName = "CaaS";
-
         public string BaseUrl { get; set; } = "";
+        public int RefreshPeriod { get; set; } = 30;
     }
 }
