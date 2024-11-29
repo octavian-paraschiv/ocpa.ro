0aa4450c521dbef8e578fddc1c24e7ecd5aff90b
Octavian Paraschiv <octavian.paraschiv@gmail.com>
2024-11-29 10:59:06 +0200

    Cat As A Service reworked x3

--- api/Helpers/Wiki/CaasHelper.cs
+++ api/Helpers/Wiki/CaasHelper.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Caching.Distributed;
 using Serilog;
 using System;
+using System.IO;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ocpa.ro.api.Helpers.Wiki
@@ -12,11 +14,20 @@ namespace ocpa.ro.api.Helpers.Wiki
         Task<byte[]> GetNewCat(string resourcePath, string queryString);
     }
 
-    public class CaasHelper : BaseHelper, ICaasHelper
+    public class CaasHelper : BaseHelper, ICaasHelper, IDisposable
     {
-        private const string CacheKey = "cat";
+        private const string CatCacheKey = "cat";
+        private const string DefaultCatImagePath = "Helpers/Wiki/loadcat.gif";
+
         private readonly HttpClient _client;
         private readonly IDistributedCache _cache;
+        private readonly ManualResetEventSlim _terminateBackgroundProcess = new ManualResetEventSlim(false);
+        private readonly ManualResetEventSlim _backgroundProcessRunning = new ManualResetEventSlim(false);
+
+        private string _resourcePath;
+        private string _queryString;
+        private bool disposedValue;
+        private readonly SemaphoreSlim _paramsSemaphore = new SemaphoreSlim(1);
 
         public CaasHelper(IWebHostEnvironment hostingEnvironment, ILogger logger,
             IHttpClientFactory factory, IDistributedCache cache)
@@ -32,46 +43,79 @@ namespace ocpa.ro.api.Helpers.Wiki
 
             try
             {
-                data = await _cache.GetAsync(CacheKey);
+                data = await _cache.GetAsync(CatCacheKey);
 
-                if (data?.Length > 0)
-                {
-                    // Fetch new cat in advance
-                    _ = FetchNewCatAsync(resourcePath, queryString).ContinueWith(task =>
-                    {
-                        if (task.IsFaulted)
-                            LogException(task.Exception);
-                    });
+                if (!(data?.Length > 0))
+                    data = await File.ReadAllBytesAsync(DefaultCatImagePath);
 
-                    return data;
-                }
+                await _paramsSemaphore.WaitAsync();
 
-                data = await FetchNewCatAsync(resourcePath, queryString);
+                _resourcePath = resourcePath;
+                _queryString = queryString;
+
+                if (!_backgroundProcessRunning.Wait(0))
+                    StartBackgroundRecacheProcess();
             }
             catch (Exception ex)
             {
                 LogException(ex);
+            }
+            finally
+            {
+                _paramsSemaphore.Release();
             }
 
             return data;
         }
 
-        private async Task<byte[]> FetchNewCatAsync(string resourcePath, string queryString)
+        private void StartBackgroundRecacheProcess()
         {
-            byte[] data = null;
-
-            try
+            ThreadPool.QueueUserWorkItem(async _ =>
             {
-                data = await _client.GetByteArrayAsync($"{resourcePath}{queryString}");
-                if (data?.Length > 0)
-                    await _cache.SetAsync(CacheKey, data);
-            }
-            catch (Exception ex)
-            {
-                LogException(ex);
-            }
+                _backgroundProcessRunning.Set();
 
-            return data;
+                while (!_terminateBackgroundProcess.Wait(0))
+                {
+                    try
+                    {
+                        string resourcePath = string.Empty;
+                        string queryString = string.Empty;
+
+                        await _paramsSemaphore.WaitAsync();
+                        resourcePath = _resourcePath;
+                        queryString = _queryString;
+                        _paramsSemaphore.Release();
+
+                        var data = await _client.GetByteArrayAsync($"{resourcePath}{queryString}");
+                        if (data?.Length > 0)
+                            await _cache.SetAsync(CatCacheKey, data);
+                    }
+                    catch (Exception ex)
+                    {
+                        LogException(ex);
+                    }
+
+                    Thread.Sleep(30000);
+                }
+            });
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                    _terminateBackgroundProcess.Set();
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
